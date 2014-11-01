39d0087a7ba642dc92e537aaad0b042766fa0b5a
basarozenboy <pashaoo@gmail.com>
2014-11-01 20:49:10 +0200

    HW-01
    Project name changed to HW-01 and encoding changed to utf-8

--- HW-01/src/simple.scala
+++ HW-01/src/simple.scala
@@ -1,9 +1,9 @@
 /**
- * Created by basar.ozenboy on 28.10.2014.
+ * Created by basar.ozenboy on 01.11.2014.
  */
 
 object simple {
-  def main(args: Array[String]): Unit =
+  def main(args: Array[String]) =
   {
     val multArray = (A: Array[Integer]) =>
     {
---
+++ _HW-01/src/simple.scala
@@ -0,0 +1,10 @@
+/**
+ * Created by basar.ozenboy on 01.11.2014.
+ */
+
+object simple {
+  def main (args: Array[String]) {
+
+  }
+
+}
