93c02ca960f7279da246655ce440259fca1c4f8b
basarozenboy <pashaoo@gmail.com>
2014-12-07 17:08:37 +0200

    HW-05

---
+++ HW-05/RandomStuff.scala
@@ -0,0 +1,74 @@
+/**
+ * Created by basar.ozenboy on 03.12.2014.
+ */
+
+trait RandomStuffTrait {
+  def transform(list: List[Int], op: (Int) => Int) : List[Int]
+  def allValid(list: List[Int], op: (Int) => Boolean) : Boolean
+  def executeWithRetry(retryCount: Int, op: => Int) : Option[Int]
+}
+
+class RandomStuff extends RandomStuffTrait {
+
+  case class RetryObj(var IntValue: Int, var BoolValue: Boolean)
+
+  override def transform(list: List[Int], op: (Int) => Int): List[Int] =
+  {
+    var newList: List[Int] = List()
+    var tmpI: Int = 0
+    for(i <- 0 to list.length - 1)
+    {
+      tmpI = op(list(i))
+      newList = insert(tmpI ,newList)
+    }
+    return newList
+  }
+
+  private def insert(x: Int, xs: List[Int]): List[Int] = {
+    xs:+ x
+  }
+
+  override def executeWithRetry(retryCount: Int, op: => Int): Option[Int] =
+  {
+    var tmpRetryObj = tryCatchOp(op)
+    if (tmpRetryObj.BoolValue == true)
+      return Option(tmpRetryObj.IntValue)
+    else
+    {
+      for(i <- 0 to retryCount - 1)
+      {
+        tmpRetryObj = tryCatchOp(op)
+        if (tmpRetryObj.BoolValue == true)
+          return Option(tmpRetryObj.IntValue)
+      }
+        return None
+    }
+  }
+
+  def tryCatchOp( op: => Int ): RetryObj =
+  {
+    var aReturnValue = RetryObj(0, false)
+    try {
+      aReturnValue.IntValue = op
+      aReturnValue.BoolValue = true
+      return aReturnValue
+    }
+    catch {
+      case e: Exception =>
+        aReturnValue.IntValue = 0
+        aReturnValue.BoolValue = false
+        return aReturnValue
+    }
+  }
+
+  override def allValid(list: List[Int], op: (Int) => Boolean): Boolean =
+  {
+    var tmpResult: Boolean = false
+    for(i <- 0 to list.length - 1) {
+      tmpResult = op(list(i))
+      if(tmpResult == false)
+        return false
+    }
+    return tmpResult
+  }
+}
